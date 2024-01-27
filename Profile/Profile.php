14df35c41c767ffe48913ac9aa4599f8c70f312a
Adimas <admsfarhan@gmail.com>
2024-01-27 20:58:11 +0700

    PROFILE UPDATE

---
+++ Profile/Profile.php
@@ -0,0 +1,135 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Profile</title>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-success" style="padding: 20px;">
+    <div class="container">
+        <a class="navbar-brand text-light mx-5" href="https://pnj.ac.id/" target="_blank" style="font-size: 24px;">Politeknik Negeri Jakarta</a>
+    </div>
+</nav>
+<body class="">
+<div class="container">
+    <div class="main-body">
+          <div class="row gutters-sm">
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="user.png" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h4>Kelompok 2</h4>
+                      <p class="text-secondary mb-1">Full Stack Mahasiswa</p>
+                      <p class="text-muted font-size-sm">Beji, Kota Depok, Indonesia</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card mt-3">
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Full Name</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                      Kelompok 2
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">NIM</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                 2207411051
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Email</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    Kelompok2@gmail.com
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Jenis Kelamin</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                     Laki-Laki
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Semester</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                     3 (Ganjil)
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Kelas</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                     Teknik Informatika B (Reguler)
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Prodi</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                     Teknik Informatika
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Jurusan</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                     Teknik Informatika 
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Nomor Telepon</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    08123123123
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <a class="btn btn-info " target="__blank" href="edit.html">Edit</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              
+
+
+
+            </div>
+          </div>
+
+        </div>
+    </div>
