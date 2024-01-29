cabe90283bab40c5706044d935d64012752f2612
Muhammad Fauzan Permana <mfauzanprmna@gmail.com>
2024-01-29 17:03:33 +0700

    Finish

--- Admin/user/detail.php
+++ Admin/user/detail.php
@@ -1,30 +1,36 @@
 <?php
+ob_start(); // Mulai buffer output
+
 include '../dashboard/atas.php';
 include '../../koneksi.php';
 
 $nip = isset($_GET['nip']) ? $_GET['nip'] : '0';
 
-$query = "SELECT * FROM dosematkul WHERE nip = '$nip'";
+$query = "SELECT dosenmatkul.*, dosen.Nama AS dosen, dosen.NIP AS NIP, matkul.Nama AS matkul, kelas.Nama AS kelas FROM dosenmatkul 
+LEFT JOIN dosen ON dosen.NIP = dosenmatkul.NIP
+LEFT JOIN matkul ON matkul.ID_matkul = dosenmatkul.ID_matkul 
+LEFT JOIN kelas ON kelas.ID_kelas = dosenmatkul.ID_kelas 
+WHERE dosenmatkul.NIP = '$nip'";
 $hasil = mysqli_query($conn, $query);
 $i = 1;
+
 
 if (isset($_GET['hapus'])) {
     $id = $_GET['hapus'];
 
     $query = "DELETE FROM dosenMatkul WHERE ID = $id";
+    $query = "DELETE FROM jadwal WHERE ID_dosenMatkul = $id";
     $hasil = mysqli_query($conn, $query);
 
     if ($hasil) {
-        echo '<div id="success-alert" class="alert alert-success alert-dismissible fade show" role="alert">
-            Data berhasil dihapus!
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>';
-        header('location:index.php');
-        echo "<script>window.location.reload();</script>";
+        header('Location: index.php');
+        exit();
     } else {
         echo "Hapus Data Gagal";
     }
 }
+
+ob_start(); // Mulai buffer output
 
 ?>
 
@@ -49,37 +55,10 @@ if (isset($_GET['hapus'])) {
                         <?php while ($data = mysqli_fetch_array($hasil)) { ?>
                             <tr>
                                 <td><?php echo $i++ ?></td>
-                                <td><?php echo $nip ?></td>
-                                <?php
-                                $query = "SELECT Nama FROM dosen WHERE nip = '$nip'";
-                                $hasil = mysqli_query($conn, $query);
-                                while ($dosen = mysqli_fetch_array($hasil)) {
-                                ?>
-
-                                    <td><?php echo $dosen['Nama'] ?></td>
-
-                                <?php } ?>
-
-                                <?php
-                                $id_matkul = $data['ID_matkul'];
-                                $query = "SELECT Nama FROM matkul WHERE ID_matkul = '$id_matkul'";
-                                $hasil = mysqli_query($conn, $query);
-                                while ($matkul = mysqli_fetch_array($hasil)) {
-                                ?>
-
-                                    <td><?php echo $matkul['Nama'] ?></td>
-
-                                <?php } ?>
-                                <?php
-                                $id_kelas = $data['ID_kelas'];
-                                $query = "SELECT Nama FROM kelas WHERE ID_kelas = '$id_kelas'";
-                                $hasil = mysqli_query($conn, $query);
-                                while ($kelas = mysqli_fetch_array($hasil)) {
-                                ?>
-
-                                    <td><?php echo $kelas['Nama'] ?></td>
-
-                                <?php } ?>
+                                <td><?php echo $data['NIP'] ?></td>
+                                <td><?php echo $data['dosen'] ?></td>
+                                <td><?php echo $data['matkul'] ?></td>
+                                <td><?php echo $data['kelas'] ?></td>
 
                                 <td>
                                     <a href="http://localhost/webprouas/admin/user/detail.php?hapus=<?php echo $data['ID'] ?>">
